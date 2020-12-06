2e27e6472e5a00a8c1c83ec0602c5c21f9df9c9a
tedi <hkanelhs@yahoo.gr>
2020-12-06 12:19:05 +0200

    Added test case for product

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .internal.enum import ColorEnum
+from .internal.enum import StoneEnum
+from .models        import Bracelet
+
+class BraceletTestCase(TestCase):
+	def setUp(self):
+		Bracelet.objects.create(
+			# Common
+			title        = "My title",
+			brief        = "My brief",
+			description  = "My description",
+			stone        = StoneEnum.LL,
+			macrame      = True,
+			color        = ColorEnum.RED,
+
+			# Bracelet
+			diameter_max = 2.5,
+			diameter_min = 1.5,
+			width_max    = 0.8,
+			width_min    = 0.3,
+			isAdjustable = True
+		)
+
+	def test_bracelet_creation(self):
+		"""Bracelets are created correctly"""
+		bracelet = Bracelet.objects.get(title="My title")
+		self.assertEqual(bracelet.title, "My title")

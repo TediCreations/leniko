95022eebca427b68003fe8a22ad916553f755970
tedi <hkanelhs@yahoo.gr>
2020-12-25 11:07:27 +0200

    Added Stones

--- products/internal/enum.py
+++ products/internal/enum.py
@@ -90,14 +90,45 @@ class FinishEnum(Enum):
 
 
 class StoneEnum(Enum):
-	N  = "None"
-	AG = "Agate"
-	LL = "Lapis Lazulis"
-	AM = "Amethyst"
-	AV = "Aventurine"
-	CA = "Carnelian"
-	CO = "Coral"
-	EM = "Emerald"
+	N                      = "None"
+	AGATE                  = "Agate"
+	AMAZONITE              = "Amazonite"
+	AMETHYST               = "Amethyst"
+	AQUAMARINE             = "Aquamarine"
+	CALCITE                = "Calcite"
+	CARNELIAN              = "Carnelian"
+	CASSITERITE            = "Cassiterite"
+	CHALCEDONY             = "Chalcedony"
+	CHRYSOCOLLA            = "Chrysocolla"
+	CHRYSOCOLLA_CHALCEDONY = "Chrysocolla Chalcedony"
+	CORAL                  = "Coral"
+	DIAMOND                = "Diamond"
+	DOLOMITE               = "Dolomite"
+	EMERALD                = "Emerald"
+	HEMATITE               = "Hematite"
+	JASPER                 = "Jasper"
+	KYANITE                = "Kyanite"
+	LABRADORITE            = "Labradorite"
+	LAPIS_LAZULI           = "Lapis Lazuli"
+	LAZULITE               = "Lazulite"
+	MAGNESITE              = "Magnesite"
+	MALACHITE              = "Malachite"
+	MOONSTONE              = "Moonstone"
+	ONYX                   = "Onyx"
+	OPAL                   = "Opal"
+	PEARL                  = "Pearl"
+	QUARTZ                 = "Quartz"
+	RHODONITE              = "Rhodonite"
+	ROSE_QUARTZ            = "Rose Quartz"
+	RUBY                   = "Ruby"
+	SMOKY_QUARTZ           = "Smoky Quartz"
+	SODALITE               = "Sodalite"
+	TIGER_S_EYE            = "Tiger's Eye"
+	TOPAZ                  = "Topaz"
+	TOURMALINE             = "Tourmaline"
+	TURQUOISE              = "Turquoise"
+	ZIRCON                 = "Zircon"
+
 
 	@staticmethod
 	def str2Enum(s):
@@ -115,14 +146,53 @@ class StoneEnum(Enum):
 
 
 class ColorEnum(Enum):
-	N  = "None"
-	RED  = "Red"
-	YEL  = "Yellow"
-	GRE  = "Green"
-	BLU  = "Blue"
-	WHI  = "White"
-	BLA  = "Black"
-	SIL  = "Silver"
+
+	N                = "None"
+	BLACK            = "Black"            #000000
+	DARK_SLATE_GRAY  = "Dark Slate Gray"  #2F4F4F
+	SLATE_GRAY       = "Slate Gray"       #708090
+	GRAY             = "Gray"             #808080
+	SILVER           = "Silver"           #C0C0C0
+	IVORY            = "Ivory"            #FFFFF0
+	BEIGE            = "Beige"            #F5F5DC
+	WHITE            = "White"            #FFFFFF
+	DARK_RED         = "Dark Red"         #A52A2A
+	SADDLE_BROWN     = "Saddle Brown"     #8B4513
+	SIENNA           = "Sienna"           #A0522D
+	DARK_BLUE        = "Dark Blue"        #00008B
+	ROYAL_BLUE       = "Royal Blue"       #4169E1
+	LIGHT_SKY_BLUE   = "Light Sky Blue"   #87CEFA
+	CADET_BLUE       = "Cadet Blue"       #5F9EA0
+	TURQUOISE        = "Turquoise"        #40E0D0
+	AQUAMARINE       = "Aquamarine"       #7FFFD4
+	LIGHT_CYAN       = "Light Cyan"       #E0FFFF
+	TEAL             = "Teal"             #008080
+	LIGHT_SEA_GREEN  = "Light Sea Green"  #20B2AA
+	DARK_SEA_GREEN   = "Dark Sea Green"   #8FBC8B
+	DARK_OLIVE_GREEN = "Dark Olive Green" #556B2F
+	OLIVE            = "Olive"            #808000
+	DARK_GREEN       = "Dark Green"       #006400
+	GREEN            = "Green"            #008000
+	FOREST_GREEN     = "Forest Green"     #228B22
+	SEA_GREEN        = "Sea Green"        #2E8B57
+	EMERALD          = "Emerald"          #50C878
+	INDIGO           = "Indigo"           #4B0082
+	PURPLE           = "Purple"           #800080
+	DARK_VIOLET      = "Dark Violet"      #9400D3
+	DARK_KHAKI       = "Dark Khaki"       #BDB76B
+	GOLD             = "Gold"             #FFD700
+	YELLOW           = "Yellow"           #FFFF00
+	ORANGE           = "Orange"           #FFA500
+	DARK_ORANGE      = "Dark Orange"      #FF8C00
+	ORANGE_RED       = "Orange Red"       #FF4500
+	CORAL            = "Coral"            #FF7F50
+	LIGHT_SALMON     = "Light Salmon"     #FFA07A
+	PINK             = "Pink"             #FFC0CB
+	FIRE_BRICK       = "Fire Brick"       #B22222
+	RED              = "Red"              #FF0000
+	SALMON           = "Salmon"           #FA8072
+	INDIAN_RED       = "Indian Red"       #CD5C5C
+
 
 	@staticmethod
 	def str2Enum(s):
@@ -131,9 +201,11 @@ class ColorEnum(Enum):
 				return e
 		return ColorEnum.N
 
+
 	def random():
 		v = [e for e in ColorEnum]
 		return random.choice(v)
+
 
 	def choices():
 		return [(e.value, e.value) for e in ColorEnum]

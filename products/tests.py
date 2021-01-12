a37f2b805e4bedd6feeca36f64533eb4db7a037b
tedi <hkanelhs@yahoo.gr>
2021-01-12 22:43:38 +0200

    Added tests to travis

--- products/tests.py
+++ products/tests.py
@@ -11,7 +11,7 @@ class BraceletTestCase(TestCase):
 			title        = "My title",
 			brief        = "My brief",
 			description  = "My description",
-			stone        = StoneEnum.LL,
+			stone        = StoneEnum.LAPIS_LAZULI,
 			macrame      = True,
 			color        = ColorEnum.RED,
 
@@ -27,3 +27,17 @@ class BraceletTestCase(TestCase):
 		"""Bracelets are created correctly"""
 		bracelet = Bracelet.objects.get(title="My title")
 		self.assertEqual(bracelet.title, "My title")
+
+
+from .internal.enum import MaterialEnum
+
+
+class MaterialEnumTestCase(TestCase):
+	def setUp(self):
+		self.a = MaterialEnum.N
+
+	def test_getName(self):
+		self.assertEqual(self.a.getName(), "None")
+
+	def test_getId(self):
+		self.assertEqual(self.a.getId(), "NO")

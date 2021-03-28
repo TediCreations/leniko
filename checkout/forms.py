fa4487451b5b57e1cbf3cb2bb9bb3eacc6e4b705
tedi <hkanelhs@yahoo.gr>
2021-03-29 01:03:57 +0300

    First steps in the checkout pProcess

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,38 +1,73 @@
 from django import forms
 
 from django_countries.fields import CountryField
-from django_countries.widgets import CountrySelectWidget
+# from django_countries.widgets import CountrySelectWidget
 
 
 PAYMENT_CHOICES = (
+	('BD', 'Bank Deposit'),
 	('CA', 'Card'),
-	('BD', 'Bank Deposit'),
-	('BD', 'Bank Deposit'),
+	('GR', 'Αντικαταβολή'),
 	('CW', 'Click away')
 )
 
 
 class BillingForm(forms.Form):
 
-	billing_address = forms.CharField(required=False)
-	billing_address2 = forms.CharField(required=False)
-	billing_country = CountryField(blank_label='(select country)').formfield(
-		required=False,
+	billing_firstName = forms.CharField(label='Name', required=True, max_length=50, initial=None)
+	billing_lastName = forms.CharField(label='Surname', required=True, max_length=50, initial=None)
+	billing_phone = forms.CharField(label='Telephone number', required=True, max_length=50, initial=None)
+	billing_email = forms.EmailField(label='Email', required=True, initial=None)
+
+	# company_name = forms.CharField(required=False, max_length=50, initial=None)
+
+	billing_address = forms.CharField(label='Address', required=True, max_length=50, initial=None)
+	billing_address2 = forms.CharField(label='Address 2', required=False, max_length=50, initial=None)
+	billing_town = forms.CharField(label='Town', required=True, max_length=50, initial=None)
+	billing_state = forms.CharField(label='State', required=False, max_length=50, initial=None)
+	"""
+	billing_country = CountryField(blank_label='Please select country').formfield(
+		required=True,
 		widget=CountrySelectWidget(attrs={
 			'class': 'custom-select d-block w-100',
 		}))
-	billing_zip = forms.CharField(required=False)
+	"""
+	billing_country = CountryField(blank_label='Please select country').formfield(required=True, label='Country')
+	billing_postalCode = forms.CharField(label='Postal code', required=True, min_length=5, max_length=5, initial=None)
 
-	same_billing_address = forms.BooleanField(required=False)
+	isShippingAddress = forms.BooleanField(label='Ship to this address?', required=False, initial=True)
+	paymentOption = forms.ChoiceField(label='Payment option', choices=PAYMENT_CHOICES, initial=None)
+
+	def store(self):
+		if self.is_valid():
+			print("\033[92mForm is VALID!!!\033[0m")
+			billing_address = self.cleaned_data['billing_address']
+			print(f"\033[92mAddress\033[0m : {billing_address}")
+		else:
+			print("\033[92mForm is VALID!!!\033[0m")
+			for e in self.errors:
+				print(f"{e}")
 
 
 class ShippingForm(forms.Form):
 
-	shipping_address = forms.CharField(required=False)
-	shipping_address2 = forms.CharField(required=False)
-	shipping_country = CountryField(blank_label='(select country)').formfield(
-		required=False,
+	shipping_firstName = forms.CharField(label='Name', required=True, max_length=50, initial=None)
+	shipping_lastName = forms.CharField(label='Surname', required=True, max_length=50, initial=None)
+	shipping_phone = forms.CharField(label='Telephone number', required=True, max_length=50, initial=None)
+	shipping_email = forms.EmailField(label='Email', required=True, initial=None)
+
+	# company_name = forms.CharField(required=False, max_length=50, initial=None)
+
+	shipping_address = forms.CharField(label='Address', required=True, max_length=50, initial=None)
+	shipping_address2 = forms.CharField(label='Address 2', required=False, max_length=50, initial=None)
+	shipping_town = forms.CharField(label='Town', required=True, max_length=50, initial=None)
+	shipping_state = forms.CharField(label='State', required=False, max_length=50, initial=None)
+	"""
+	shipping_country = CountryField(blank_label='Please select country').formfield(
+		required=True,
 		widget=CountrySelectWidget(attrs={
 			'class': 'custom-select d-block w-100',
 		}))
-	shipping_zip = forms.CharField(required=False)
+	"""
+	shipping_country = CountryField(blank_label='Please select country').formfield(required=True, label='Country')
+	shipping_postalCode = forms.CharField(label='Postal code', required=True, min_length=5, max_length=5, initial=None)
--- checkout/views.py
+++ checkout/views.py
@@ -6,13 +6,19 @@ from django.views import View
 from django_countries import countries
 
 from cart.models import Cart
-from checkout.models import CheckOutEnum
+from .models import CheckOutEnum
+
+from .forms import BillingForm
+from .forms import ShippingForm
+
 from pages.apps import PagesConfig
 
 
 theme = PagesConfig.theme
 
 CHECKOUTSTEP_SESSION_ID = "checkoutStep"
+BILLINGFORM_SESSION_ID = "billingForm"
+SHIPPINGFORM_SESSION_ID = "shippingForm"
 
 
 class CheckOutManager(object):
@@ -36,12 +42,76 @@ class CheckOutManager(object):
 		self.state = state
 
 	def handle(self):
-		self._writeSession(CheckOutEnum.BILLING)
+		print(f"State = \033[92m{self.state.value}\033[0m")
 
-		# if self.state == CheckOutEnum.BILLING:
-		# 	self._writeSession(CheckOutEnum.CONFIRM)
-		# else:
-		# 	self._writeSession(CheckOutEnum.BILLING)
+		if self.state == CheckOutEnum.N:
+			self._writeSession(CheckOutEnum.BILLING)
+		elif self.state == CheckOutEnum.BILLING:
+			form = BillingForm(self.request.POST)
+			if form.is_valid():
+				# Save form
+				self.request.session['BILLINGFORM_SESSION_ID'] = form.cleaned_data
+				self.request.session.modified = True
+
+				# Get cleaned data fromthe form
+				billing = dict()
+				billing['firstName'] = form.cleaned_data['billing_firstName']
+				billing['lastName'] = form.cleaned_data['billing_lastName']
+				billing['phone'] = form.cleaned_data['billing_phone']
+				billing['email'] = form.cleaned_data['billing_email']
+				billing['address'] = form.cleaned_data['billing_address']
+				billing['address2'] = form.cleaned_data['billing_address2']
+				billing['town'] = form.cleaned_data['billing_town']
+				billing['state'] = form.cleaned_data['billing_state']
+				billing['country'] = form.cleaned_data['billing_country']
+				billing['postalCode'] = form.cleaned_data['billing_postalCode']
+
+				billing['isShippingAddress'] = form.cleaned_data['isShippingAddress']
+				billing['paymentOption'] = form.cleaned_data['paymentOption']
+
+				if billing['isShippingAddress'] is True:
+
+					# Create shipping form
+					shipping = dict()
+					shipping['shipping_firstName'] = billing['firstName']
+					shipping['shipping_lastName'] = billing['lastName']
+					shipping['shipping_phone'] = billing['phone']
+					shipping['shipping_email'] = billing['email']
+					shipping['shipping_address'] = billing['address']
+					shipping['shipping_address2'] = billing['address2']
+					shipping['shipping_town'] = billing['town']
+					shipping['shipping_state'] = billing['state']
+					shipping['shipping_country'] = billing['country']
+					shipping['shipping_postalCode'] = billing['postalCode']
+
+					shippingForm = ShippingForm(initial=shipping)
+					if shippingForm.is_valid():
+						# Save form
+						self.request.session['SHIPPING_SESSION_ID'] = shippingForm.cleaned_data
+						self.request.session.modified = True
+
+						# We can skip shipping page
+						self._writeSession(CheckOutEnum.CONFIRM)
+					else:
+						# Critical error
+						print("\033[91mERROR ERROR ERROR ERROR ERROR ERROR ERROR !!!!!!!!!!!!!!!!!!!!\033[0m")
+						# print(f"Form:   {shippingForm}")
+						print(f"Errors: {shippingForm.errors}")
+						print(f"Errors: {shippingForm.non_field_errors()}")
+				else:
+					# We can not skip the shiping page
+					self._writeSession(CheckOutEnum.SHIPPING)
+
+				# Debug
+				for key in billing:
+					print(f"B: {key} = {billing[key]}")
+				#for key in shipping:
+				#	print(f"S: {key} = {shipping[key]}")
+
+			else:
+				print(f"\033[91mERROR in FORM\033[0m | {form.errors}")
+		else:
+			print("\033[91mState not coded yet!!!!!!!!!!!\033[0m")
 
 	def debug(self, text=None):
 		print('----------------------------------')
@@ -54,7 +124,7 @@ class CheckOutManager(object):
 
 		print('SESSION')
 		for key, value in self.request.session.items():
-			print(f'{key}: {value}')
+			print(f'{key}: {value} | type={type(key)}')
 		print("")
 
 
@@ -80,19 +150,22 @@ class CheckoutView(View):
 
 		manager = CheckOutManager(request)
 
-		manager.debug("GET PRE")
+		# manager.debug("GET PRE")
 		checkoutState = manager.getState()
-		manager.debug("GET POST")
+		# manager.debug("GET POST")
 
 		# Decide view
 		if checkoutState == CheckOutEnum.N:
 			return HttpResponseRedirect(reverse("cart:detail"))
 		if checkoutState == CheckOutEnum.BILLING:
 			template_name = theme + '/checkout/billing.html'
+			form = BillingForm(initial=self.request.session.get('BILLINGFORM_SESSION_ID'))
 		elif checkoutState == CheckOutEnum.SHIPPING:
 			template_name = theme + '/checkout/shipping.html'
+			form = ShippingForm(initial=self.request.session.get('SHIPPINGFORM_SESSION_ID'))
 		elif checkoutState == CheckOutEnum.CONFIRM:
 			template_name = theme + '/checkout/confirm.html'
+			form = None  # ConfirmForm(initial=self.request.session.get('confirmForm'))
 		elif checkoutState == CheckOutEnum.SUCCESS:
 			return HttpResponseRedirect(reverse("products:list"))
 		elif checkoutState == CheckOutEnum.FAIL:
@@ -110,7 +183,8 @@ class CheckoutView(View):
 			"webpage_name": self.webpage_name,
 			"webpage_description": self.webpage_description,
 			"cart": cart,
-			"countries": countries
+			"countries": countries,
+			"form": form
 		}
 
 		return render(request, template_name, context)
@@ -120,5 +194,5 @@ class CheckoutView(View):
 		manager = CheckOutManager(request)
 		manager.debug("POST PRE")
 		manager.handle()
-		manager.debug("POST POST")
+		# manager.debug("POST POST")
 		return HttpResponseRedirect(reverse("checkout:checkout"))

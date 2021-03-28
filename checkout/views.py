72024b41fc7761a5b7be6f7c7d515613c0d5df87
tedi <hkanelhs@yahoo.gr>
2021-03-28 14:34:18 +0300

    Added checkout state handler

--- checkout/views.py
+++ checkout/views.py
@@ -31,38 +31,44 @@ class CheckoutView(View):
 	webpage_name = "Checkout"
 	webpage_description = "Leniko jewelry checkout page"
 
-	def get(self, request):
+	def _getCheckOutStep(self, request):
 
 		# print('GET')
 		# for key, value in request.GET.items():
 		# 	print(f'{key}: {value}')
 
-		print('SESSION')
-		for key, value in request.session.items():
-			print(f'{key}: {value}')
+		# print('SESSION')
+		# for key, value in request.session.items():
+		# 	print(f'{key}: {value}')
 
 		# page = request.GET.get('p')
-		page = request.session.get(CHECKOUTSTEP_SESSION_ID)
 
-		if not page:
-			# page =
+		reachedCheckOutStep = request.session.get(CHECKOUTSTEP_SESSION_ID)
+
+		if not reachedCheckOutStep:
 			request.session[CHECKOUTSTEP_SESSION_ID] = "b"
-			request.session.modified = True
-			page = "b"
+			reachedCheckOutStep = "b"
 
-		if page == "b":
+		if reachedCheckOutStep == "b":
 			request.session[CHECKOUTSTEP_SESSION_ID] = "s"
-			request.session.modified = True
 			template_name = theme + '/checkout/billing.html'
-		elif page == "s":
+		elif reachedCheckOutStep == "s":
 			request.session[CHECKOUTSTEP_SESSION_ID] = "c"
-			request.session.modified = True
 			template_name = theme + '/checkout/shipping.html'
-		elif page == "c":
+		elif reachedCheckOutStep == "c":
 			request.session[CHECKOUTSTEP_SESSION_ID] = "f"
-			request.session.modified = True
 			template_name = theme + '/checkout/confirm.html'
 		else:
+			template_name = None
+
+		request.session.modified = True
+		return template_name
+
+	def get(self, request):
+
+		# Get checkout step
+		template_name = self._getCheckOutStep(request)
+		if not template_name:
 			return HttpResponseRedirect(reverse("products:list"))
 
 		# --------------------------------------------------
@@ -77,22 +83,6 @@ class CheckoutView(View):
 		print("PRE SESSION")
 		print(request.session.items())
 		print("POST SESSION")
-
-		# --------------------------------------------------
-		# Get checkout step
-		# request.session.modified = True
-		# self.session = request.session
-		# checkoutStep = self.session.get(CHECKOUTSTEP_SESSION_ID)
-		# if not checkoutStep:
-		# 	# save first
-		# 	checkoutStep = self.session[CHECKOUTSTEP_SESSION_ID] = {}
-		# 	checkoutStep["id"] = str(1)
-		# step = checkoutStep["id"]
-
-		# print('GET')
-		# for key, value in request.GET.items():
-		# 	print(f'{key}: {value}')
-		# print(f'step: {step}')
 
 		# --------------------------------------------------
 		# Render
